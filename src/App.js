77b8b149b7465485d9ce05bb620ef936e551aa84
frayz <andrydmit@gmail.com>
2019-12-28 00:29:10 +0200

    Working with navbar

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import Container from '@material-ui/core/Container';
+import Navigation from 'modules/Navigation';
+
+function App() {
+  return (
+    <>
+      <Navigation />
+      <Container>
+        <div>opopop</div>
+
+        {/* <Navbar />
+      <Hero />
+      <Carousel />
+      <Skills />
+      <Contacts />
+      <Footer /> */}
+      </Container>
+    </>
+  );
+}
+
+export default App;
