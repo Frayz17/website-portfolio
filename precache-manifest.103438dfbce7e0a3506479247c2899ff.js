2e221c3649eae910cb7033015e8a58aec0750eb0
Andrey Dmitriev <andrydmit@gmail.com>
2020-01-10 23:17:30 +0200

    Updates

---
+++ precache-manifest.103438dfbce7e0a3506479247c2899ff.js
@@ -0,0 +1,154 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "36eb49312746d61239af1a0bd11a32dd",
+    "url": "/website-portfolio/index.html"
+  },
+  {
+    "revision": "75a2885e9be6ef195cb3",
+    "url": "/website-portfolio/static/css/2.bc95f848.chunk.css"
+  },
+  {
+    "revision": "75a2885e9be6ef195cb3",
+    "url": "/website-portfolio/static/js/2.fd7f4578.chunk.js"
+  },
+  {
+    "revision": "374a015f9bd7dbb4f757a46f1c6a7946",
+    "url": "/website-portfolio/static/js/2.fd7f4578.chunk.js.LICENSE"
+  },
+  {
+    "revision": "37d8f75d3986fe893666",
+    "url": "/website-portfolio/static/js/main.08c1fabd.chunk.js"
+  },
+  {
+    "revision": "ae502af3aa2532f95465",
+    "url": "/website-portfolio/static/js/runtime-main.d9c1e297.js"
+  },
+  {
+    "revision": "4fd7954596f268d316cd8d342e16d8c8",
+    "url": "/website-portfolio/static/media/forPortfolio.4fd79545.jpg"
+  },
+  {
+    "revision": "f0d16bdd1bb3e0df1065fa8d48f308aa",
+    "url": "/website-portfolio/static/media/hotel_resort_250.f0d16bdd.jpg"
+  },
+  {
+    "revision": "b67ff41f573b9a8caedeeaa7f6597d25",
+    "url": "/website-portfolio/static/media/lambda_restaurant_250.b67ff41f.jpg"
+  },
+  {
+    "revision": "94548b1bcc1c441d6fed33b62e9d12dd",
+    "url": "/website-portfolio/static/media/mishka_250.94548b1b.jpg"
+  },
+  {
+    "revision": "ca5973b3699dbffe3199282aff310685",
+    "url": "/website-portfolio/static/media/nighties_store_250.ca5973b3.jpg"
+  },
+  {
+    "revision": "4a78c23653900543ac02f22f39ea148d",
+    "url": "/website-portfolio/static/media/restourant_whiteFish_250.4a78c236.jpg"
+  },
+  {
+    "revision": "5cb7edfceb233100075dc9a1e12e8da3",
+    "url": "/website-portfolio/static/media/roboto-latin-100.5cb7edfc.woff"
+  },
+  {
+    "revision": "7370c3679472e9560965ff48a4399d0b",
+    "url": "/website-portfolio/static/media/roboto-latin-100.7370c367.woff2"
+  },
+  {
+    "revision": "f8b1df51ba843179fa1cc9b53d58127a",
+    "url": "/website-portfolio/static/media/roboto-latin-100italic.f8b1df51.woff2"
+  },
+  {
+    "revision": "f9e8e590b4e0f1ff83469bb2a55b8488",
+    "url": "/website-portfolio/static/media/roboto-latin-100italic.f9e8e590.woff"
+  },
+  {
+    "revision": "b00849e00f4c2331cddd8ffb44a6720b",
+    "url": "/website-portfolio/static/media/roboto-latin-300.b00849e0.woff"
+  },
+  {
+    "revision": "ef7c6637c68f269a882e73bcb57a7f6a",
+    "url": "/website-portfolio/static/media/roboto-latin-300.ef7c6637.woff2"
+  },
+  {
+    "revision": "14286f3ba79c6627433572dfa925202e",
+    "url": "/website-portfolio/static/media/roboto-latin-300italic.14286f3b.woff2"
+  },
+  {
+    "revision": "4df32891a5f2f98a363314f595482e08",
+    "url": "/website-portfolio/static/media/roboto-latin-300italic.4df32891.woff"
+  },
+  {
+    "revision": "479970ffb74f2117317f9d24d9e317fe",
+    "url": "/website-portfolio/static/media/roboto-latin-400.479970ff.woff2"
+  },
+  {
+    "revision": "60fa3c0614b8fb2f394fa29944c21540",
+    "url": "/website-portfolio/static/media/roboto-latin-400.60fa3c06.woff"
+  },
+  {
+    "revision": "51521a2a8da71e50d871ac6fd2187e87",
+    "url": "/website-portfolio/static/media/roboto-latin-400italic.51521a2a.woff2"
+  },
+  {
+    "revision": "fe65b8335ee19dd944289f9ed3178c78",
+    "url": "/website-portfolio/static/media/roboto-latin-400italic.fe65b833.woff"
+  },
+  {
+    "revision": "020c97dc8e0463259c2f9df929bb0c69",
+    "url": "/website-portfolio/static/media/roboto-latin-500.020c97dc.woff2"
+  },
+  {
+    "revision": "87284894879f5b1c229cb49c8ff6decc",
+    "url": "/website-portfolio/static/media/roboto-latin-500.87284894.woff"
+  },
+  {
+    "revision": "288ad9c6e8b43cf02443a1f499bdf67e",
+    "url": "/website-portfolio/static/media/roboto-latin-500italic.288ad9c6.woff"
+  },
+  {
+    "revision": "db4a2a231f52e497c0191e8966b0ee58",
+    "url": "/website-portfolio/static/media/roboto-latin-500italic.db4a2a23.woff2"
+  },
+  {
+    "revision": "2735a3a69b509faf3577afd25bdf552e",
+    "url": "/website-portfolio/static/media/roboto-latin-700.2735a3a6.woff2"
+  },
+  {
+    "revision": "adcde98f1d584de52060ad7b16373da3",
+    "url": "/website-portfolio/static/media/roboto-latin-700.adcde98f.woff"
+  },
+  {
+    "revision": "81f57861ed4ac74741f5671e1dff2fd9",
+    "url": "/website-portfolio/static/media/roboto-latin-700italic.81f57861.woff"
+  },
+  {
+    "revision": "da0e717829e033a69dec97f1e155ae42",
+    "url": "/website-portfolio/static/media/roboto-latin-700italic.da0e7178.woff2"
+  },
+  {
+    "revision": "9b3766ef4a402ad3fdeef7501a456512",
+    "url": "/website-portfolio/static/media/roboto-latin-900.9b3766ef.woff2"
+  },
+  {
+    "revision": "bb1e4dc6333675d11ada2e857e7f95d7",
+    "url": "/website-portfolio/static/media/roboto-latin-900.bb1e4dc6.woff"
+  },
+  {
+    "revision": "28f9151055c950874d2c6803a39b425b",
+    "url": "/website-portfolio/static/media/roboto-latin-900italic.28f91510.woff"
+  },
+  {
+    "revision": "ebf6d1640ccddb99fb49f73c052c55a8",
+    "url": "/website-portfolio/static/media/roboto-latin-900italic.ebf6d164.woff2"
+  },
+  {
+    "revision": "bab7aacd44d08147a1f2284927569c0d",
+    "url": "/website-portfolio/static/media/technomart_250.bab7aacd.jpg"
+  },
+  {
+    "revision": "fc5ee4c9ca64992869961ee5ca98a194",
+    "url": "/website-portfolio/static/media/todo_list_vuejs_250.fc5ee4c9.png"
+  }
+]);
