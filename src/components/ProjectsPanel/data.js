348c14938780548595e29f6cf957028996a16b04
Andrey <andrydmit@gmail.com>
2020-01-11 11:29:29 +0200

    Sun Foods project added.

--- src/components/ProjectsPanel/data.js
+++ src/components/ProjectsPanel/data.js
@@ -5,6 +5,7 @@ import nightiesStore from 'static/images/projects/nighties_store_250.jpg';
 import restourantWhiteFish from 'static/images/projects/restourant_whiteFish_250.jpg';
 import technomart from 'static/images/projects/technomart_250.jpg';
 import todoListVue from 'static/images/projects/todo_list_vuejs_250.png';
+import sunFoods from 'static/images/projects/sun_foods_250.jpg';
 
 const data = [
   {
@@ -17,14 +18,6 @@ const data = [
   },
   {
     id: 2,
-    title: 'Lambda Restourant',
-    description:
-      'HTML and CSS markup of this website was developed by me. The design was made by Oxygenna, designer agency from Crete, Greece.',
-    imgSrc: lambdaRestaurant,
-    href: 'https://frayz17.github.io/lambdaRestaurant/index.html'
-  },
-  {
-    id: 3,
     title: 'Nighties Store',
     description:
       'Online Store of pajamas and nighties wear. Written in WooCommerce.',
@@ -32,12 +25,20 @@ const data = [
     href: 'https://nighties.com.ua/'
   },
   {
-    id: 4,
+    id: 3,
     title: 'ToDo List',
     description:
       'Created using Vuejs, axios as http client and JSONPlaceholder API for data manipulation.',
     imgSrc: todoListVue,
     href: 'https://frayz17.github.io/todo-vue/'
+  },
+  {
+    id: 4,
+    title: 'Lambda Restourant',
+    description:
+      'HTML and CSS markup of this website was developed by me. The design was made by Oxygenna, designer agency from Crete, Greece.',
+    imgSrc: lambdaRestaurant,
+    href: 'https://frayz17.github.io/lambdaRestaurant/index.html'
   },
   {
     id: 5,
@@ -49,6 +50,14 @@ const data = [
   },
   {
     id: 6,
+    title: 'Sun Foods',
+    description:
+      'HTML and CSS markup of this website was developed by me. Designed by makebecool web studio.',
+    imgSrc: sunFoods,
+    href: 'https://frayz17.github.io/sun-foods/'
+  },
+  {
+    id: 7,
     title: 'Mishka',
     description:
       'HTML and CSS markup of this website was developed by me. The design was made by HTML Academy',
@@ -56,7 +65,7 @@ const data = [
     href: 'https://frayz17.github.io/Mishka-magazine-htmlAcademy/'
   },
   {
-    id: 7,
+    id: 8,
     title: 'Technomart',
     description:
       'HTML and CSS markup of this website was developed by me. The design was made by HTML Academy',
